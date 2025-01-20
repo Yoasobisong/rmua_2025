c5c80e31397ec790f82b9bfae7eb383981cd6dd6
Yoasobisong <3133824384@qq.com>
2025-01-20 12:45:41 +0800

    update yolo_deal.py

--- drone_ws/src/navigation_vision/scripts/yolo_deal.py
+++ drone_ws/src/navigation_vision/scripts/yolo_deal.py
@@ -10,12 +10,16 @@ import numpy as np
 from navigation_vision.msg import VisionYolo, Box
 
 class YOLODetector:
-    def __init__(self, conf=0.35):
+    def __init__(self, conf=0.35, radio_k=0.5):
         # Initialize ROS node
         rospy.init_node('yolo_detector', anonymous=True)
         self.conf = conf
         self.classes = ["left_door", "right_door", "car"]
         # Load YOLO model
+        self.highest_door_ = None
+        self.radio_k_ = radio_k
+        self.highest_door_position_ = Box()
+        self.target_position_ = Box()
         self.model = YOLO('/home/socrates/workspace/rmru_2025/yolov8/runs/rmru_train_22/weights/rmru_l.pt')
         
         # Subscribe to image topic
@@ -24,6 +28,7 @@ class YOLODetector:
         # Create publisher for processed image
         self.image_pub = rospy.Publisher('/airsim_node/drone_1/front_right/vision_detection', Image, queue_size=1)
         self.yolo_pub = rospy.Publisher('/airsim_node/drone_1/front_right/vision_result', VisionYolo, queue_size=1)
+        self.target_pub = rospy.Publisher('/airsim_node/drone_1/front_right/target_position', Box, queue_size=1)
         rospy.loginfo("YOLODetector initialized")
 
     def image_callback(self, msg):
@@ -44,26 +49,6 @@ class YOLODetector:
             confidences = results[0].boxes.conf.cpu().numpy()
             # print(f"xy: {xy}, classes: {classes}, con: {confidences}")
             # print(f" {type(xy)}, {type(classes)}, {type(confidences)}")
-            # Visualize the results on the image with custom parameters
-            annotated_frame = result.plot(
-                line_width=1,      # Set box line width to 1
-                font_size=8,       # Set font size to 8
-                boxes=True,        # Show boxes
-                labels=True,       # Show labels
-                conf=True          # Show confidence scores
-            )
-            
-            # Convert back to ROS Image message
-            img_msg = Image()
-            img_msg.header = msg.header
-            img_msg.height = annotated_frame.shape[0]
-            img_msg.width = annotated_frame.shape[1]
-            img_msg.encoding = 'bgr8'
-            img_msg.is_bigendian = False
-            img_msg.step = annotated_frame.shape[1] * 3
-            img_msg.data = annotated_frame.tobytes()
-            # Publish the processed image
-            self.image_pub.publish(img_msg)
 
             result_msg = VisionYolo()
             result_msg.header.stamp = rospy.Time.now()
@@ -83,10 +68,59 @@ class YOLODetector:
             result_msg.class_names = [self.classes[int(name)] for name in classes]
             # Publish the result message
             self.yolo_pub.publish(result_msg)
-                
+            
+            # Get highest confidence detection excluding 'car' class
+            confidences = np.array(result_msg.confidences)
+            valid_indices = [i for i, name in enumerate(result_msg.class_names) if name != 'car']
+            # rospy.loginfo(f"valid_indices: {valid_indices}")
+            if valid_indices:
+                max_idx = valid_indices[np.argmax(confidences[valid_indices])]
+                self.highest_door_ = result_msg.class_names[max_idx]
+                self.highest_door_position_ = result_msg.boxes[max_idx]
+                if self.highest_door_ == "left_door":
+                    self.target_position_.box[0] = self.highest_door_position_.box[0] + (0.5 + self.radio_k_) * self.highest_door_position_.box[2]
+                    self.target_position_.box[1] = self.highest_door_position_.box[1] + 0.5* self.highest_door_position_.box[3]
+                elif self.highest_door_ == "right_door":
+                    self.target_position_.box[0] = self.highest_door_position_.box[0] - (0.5 + self.radio_k_) * self.highest_door_position_.box[2]
+                    self.target_position_.box[1] = self.highest_door_position_.box[1] + 0.5* self.highest_door_position_.box[3]
+                else:
+                    self.target_position_.box[0] = 0
+                    self.target_position_.box[1] = 0
+            else:
+                rospy.loginfo("No valid detections found (excluding car)")
+                self.target_position_.box[0] = 0
+                self.target_position_.box[1] = 0
+                self.target_position_.box[2] = 0
+                self.target_position_.box[3] = 0
+            rospy.loginfo(f"target_position_: {self.target_position_}") 
+            self.target_pub.publish(self.target_position_)
+
+            # draw the result
+            annotated_frame = result.plot(
+                line_width=1,      # Set box line width to 1
+                font_size=8,       # Set font size to 8
+                boxes=True,        # Show boxes
+                labels=True,       # Show labels
+                conf=True          # Show confidence scores
+            )
+            # draw the target position
+            cv2.circle(annotated_frame, (int(self.target_position_.box[0]), int(self.target_position_.box[1])), 5, (0, 0, 255), -1)
+            # Convert back to ROS Image message
+            img_msg = Image()
+            img_msg.header = msg.header
+            img_msg.height = annotated_frame.shape[0]
+            img_msg.width = annotated_frame.shape[1]
+            img_msg.encoding = 'bgr8'
+            img_msg.is_bigendian = False
+            img_msg.step = annotated_frame.shape[1] * 3
+            img_msg.data = annotated_frame.tobytes()
+            # Publish the processed image
+            self.image_pub.publish(img_msg)
+
         except Exception as e:
             rospy.logerr(f"Error processing image: {str(e)}")
-
+        
+        
     def run(self):
         try:
             rospy.spin()
